6d06dbcb21609fc21fdaad7004a086dcbefd088b
Renan Steiger <renansteiger@live.com>
2023-05-03 19:06:10 -0300

    feat: Add value obj
    -Add abstract value object\n -Add value object for entity ID

--- src/category/@seedwork/domain/value-objects/__tests__/unique-entity-id.vo.spec.ts
+++ src/category/@seedwork/domain/value-objects/__tests__/unique-entity-id.vo.spec.ts
@@ -1,24 +1,27 @@
-import invalidUuidError from "../errors/invalid-uuid.error"
-import UniqueEntityId from "./unique-entity-id.vo"
+import invalidUuidError from "../../../errors/invalid-uuid.error"
+import UniqueEntityId from "../unique-entity-id.vo"
 
+function spyValidateMethod() {
+  return jest.spyOn(UniqueEntityId.prototype as any, 'validate')
+}
 describe("UniqueEntityId Unit Tests", () => {
   it('should throw error when uuid is invalid', () => {
-    const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, 'validate')
+    const validateSpy = spyValidateMethod()
     expect(() => new UniqueEntityId("fake id")).toThrow(new invalidUuidError())
     expect(validateSpy).toHaveBeenCalled()
   })
   it('should accept a uuid passed in constructor', () => {
-    const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, 'validate')
+    const validateSpy = spyValidateMethod()
     const uuid = "31591f48-a70d-455d-828e-b41a53ff0c90"
     const vo = new UniqueEntityId(uuid)
-    expect(vo.id).toBe(uuid)
+    expect(vo.value).toBe(uuid)
     expect(validateSpy).toHaveBeenCalled()
   })
 
   it('should accept a uuid passed in constructor', () => {
-    const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, 'validate')
+    const validateSpy = spyValidateMethod()
     const vo = new UniqueEntityId()
-    expect(vo.id).toBeTruthy()
+    expect(vo.value).toBeTruthy()
     expect(validateSpy).toHaveBeenCalled()
   })
 })
